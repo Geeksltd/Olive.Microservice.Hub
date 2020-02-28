91858eb4fdbae1f6ac078a43d472266be0afb82f
mrp_geeksltd <mrp@geeks.ltd.uk>
2020-02-28 13:41:45 +0000

    Add Domain project and init Nuget packages

---
+++ Domain/-Logic/ColourPalette.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace Domain
+{
+    public class ColourPalette
+    {
+        static readonly string[] colours = new[] { "#CC3542", "#FA7902", "#FAB320", "#2CC6D2", "#0A98CF", "#065280" };
+
+        static Stack colourStack = new Stack(colours);
+
+        public static string GetColourCode()
+        {
+            if (colourStack.Count <= 0)
+            {
+                for (var i = 0; i < colours.Length; i++)
+                    colourStack.Push(colours[i]);
+            }
+
+            return colourStack.Pop().ToString();
+        }
+    }
+}
